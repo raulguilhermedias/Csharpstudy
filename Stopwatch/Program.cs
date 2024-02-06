eb3e9d6a7e34ebc9b229b0b8d32be9c15a1b6550
Raul Guilherme Dias <raulguilhermedias@gmail.com>
2024-02-06 13:13:26 -0300

    c

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,18 +1,73 @@
 ﻿using System;
+using System.Threading;
 
 namespace StopWatch
 {
-    class Program{
-        static void Main(string[] args){
-
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
         }
 
-        static void Start(){
-            int time = 10;
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 10m = 10 minutos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo Deseja contar?");
+            Console.WriteLine("----------------------");
 
-            while(){
-                
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
             }
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+
         }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!!");
+            Thread.Sleep(2000);
+
+            Start(time);
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+
+                currentTime++;
+
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(2500);
+            Menu();
+        }
+
+
     }
 }
