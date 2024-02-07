86151afc1f554f9b331b7ce3aa249c9bce9ae747
Raul Guilherme Dias <raulguilhermedias@gmail.com>
2024-02-07 10:21:05 -0300

    c

---
+++ Guids/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Interpolação de Strings:
+
+            //var texto = "o preço do produto é: " + price + " apenas na promoção!";
+            // var texto = string.Format("O preço do produto é: {0} apenas na promoção {1}", price, true);
+            var price = 10.2;
+            var texto = $"O preço do produto é: \n {price}"; // favorito *
+
+            Console.WriteLine(texto);
+
+
+            // var id = Guid.NewGuid();
+            // id.ToString();
+
+            // id = new Guid("eb2097f0-16a2-42e8-acf0-c8b86426b897");
+
+            // id = new Guid();
+            // Console.WriteLine(id.ToString().Substring());
+
+        }
+    }
+}
--- TextEditor/obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
+++ TextEditor/obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386301d218873d3764a40e4fa063c7c2de01a2a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fc4581a54c8fbf065b0bdf519c092f77d14b05")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
