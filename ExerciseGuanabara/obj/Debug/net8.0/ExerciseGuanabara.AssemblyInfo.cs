346b664bd15d180eb822cc1abf3f08e00bd68ea2
Raul Guilherme Dias <raulguilhermedias@gmail.com>
2024-02-16 14:20:23 -0300

    c

--- ExerciseGuanabara/obj/Debug/net8.0/ExerciseGuanabara.AssemblyInfo.cs
+++ ExerciseGuanabara/obj/Debug/net8.0/ExerciseGuanabara.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExerciseGuanabara")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5990613aa75fe1f5ff5984ce7e6da17213dd18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718c4de1ef026fa6e4c644ce2a0aa2f93d3fdb98")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExerciseGuanabara")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExerciseGuanabara")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
